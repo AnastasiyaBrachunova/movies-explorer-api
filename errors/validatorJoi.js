3bf72a4f068f545739124238a93620c1345bf5b5
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-10-07 23:30:35 +0300

    validator

--- errors/validatorJoi.js
+++ errors/validatorJoi.js
@@ -3,7 +3,7 @@ const validator = require('validator');
 
 const validateCreateUser = celebrate({ // signup
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),

6ce7febf82730f35cf982ee89dd7fb70d03aa852
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-10-08 02:06:57 +0300

    back commit and change delmovie

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const options = require('./utils/cors');
 const limiter = require('./utils/rateLimiter');
-const MONGO_DB = require('./utils/config');
+// const MONGO_DB = require('./utils/config');
 
 // const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger'); // ИМПОРТ ЛОГОВ
@@ -30,12 +30,12 @@ app.use(helmet()); // устанавливаt различные HTTP-загол
 const {
   PORT = 3000,
   // MONGO_PROD,
-  // NODE_ENV = 'develop',
+  // NODE_ENV,
 } = process.env;
 
-// mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
-mongoose.connect(MONGO_DB);
+// mongoose.connect(MONGO_DB);
 
 app.use('*', cors(options));
 

d5f6f512b7340ede04e6f2d7cccf3a82931beb46
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-10-07 11:56:11 +0300

    Первое ревью

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new AuthorizationError('Ошибка авторизации'));
+    next(new AuthorizationError('Ошибка авторизации-222'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +15,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new AuthorizationError('Ошибка авторизации - 1'));
+    next(new AuthorizationError('Ошибка авторизации - 222'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
